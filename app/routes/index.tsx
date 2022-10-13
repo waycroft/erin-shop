520849bacc46908c95d2768f91270f92fce33899
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2022-10-12 19:41:13 -0700

    under construction...

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,10 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Erin Hoffman: Shop</h1>
-      <p>(Coming soon)</p>
+    <div className="container mx-auto h-screen flex flex-col justify-center">
+      <div className="h-fit text-center">
+        <h1>Erin Hoffman: Shop</h1>
+        <p>(Coming soon)</p>
+      </div>
     </div>
   );
 }
