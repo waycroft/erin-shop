8c7435aeda739ec3c53cb25cdd9329c8b45ad2c1
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2022-10-12 23:02:23 -0700

    nav component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,10 +1,17 @@
+import PrimaryNav from "~/components/PrimaryNav";
+
 export default function Index() {
   return (
-    <div className="container mx-auto h-screen flex flex-col justify-center">
-      <div className="h-fit text-center">
-        <h1>Erin Hoffman: Shop</h1>
-        <p>(Coming soon)</p>
-      </div>
+    <div className="container mx-auto h-screen flex flex-col">
+      <section>
+        <PrimaryNav />
+      </section>
+      <section className="flex flex-col h-full place-content-center">
+        <div className="h-fit text-center">
+          <h1>Erin Hoffman: Shop</h1>
+          <p>(Coming soon)</p>
+        </div>
+      </section>
     </div>
   );
 }
