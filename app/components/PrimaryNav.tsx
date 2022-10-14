7dc1526fa258d720df8bd7284b6d9197a17f06d0
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2022-10-13 22:54:38 -0700

    responsive hamburger nav fix

--- app/components/PrimaryNav.tsx
+++ app/components/PrimaryNav.tsx
@@ -11,32 +11,34 @@ export default function PrimaryNav() {
       <div>
         <Link to="/">ERIN HOFFMAN: SHOP</Link>
       </div>
-      <div className="md:flex flex-row hidden">
-        <div className="px-4">
-          <a href="https://erinphoffman.com/work" target="_blank">
-            Work
-          </a>
-        </div>
-        <div className="px-4">
-          <a href="https://erinphoffman.com/about" target="_blank">
-            About
-          </a>
-        </div>
-        <div className="px-4">
-          <a href="https://erinphoffman.com" target="_blank">
-            erinphoffman.com
-          </a>
+      <div>
+        <div>
+          <button className="md:hidden btn btn-square btn-ghost">
+            <HamburgerIcon />
+          </button>
+          <div className="hidden md:flex flex-row">
+            <div className="px-4">
+              <a href="https://erinphoffman.com/work" target="_blank">
+                Work
+              </a>
+            </div>
+            <div className="px-4">
+              <a href="https://erinphoffman.com/about" target="_blank">
+                About
+              </a>
+            </div>
+            <div className="px-4">
+              <a href="https://erinphoffman.com" target="_blank">
+                erinphoffman.com
+              </a>
+            </div>
+          </div>
         </div>
         <div className="px-4">
           <label htmlFor="cart-drawer" className="drawer-button btn btn-circle">
             <CartIcon />
           </label>
         </div>
-      </div>
-      <div>
-        <button className="btn btn-square btn-ghost md:hidden">
-          <HamburgerIcon />
-        </button>
       </div>
     </nav>
   );
