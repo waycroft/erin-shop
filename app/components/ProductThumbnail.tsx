4392d2c733ed0e01a38ff67e0a901eb3f845ddf1
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2022-10-15 16:15:49 -0700

    buttons overlay on product thumbnail on hover

--- app/components/ProductThumbnail.tsx
+++ app/components/ProductThumbnail.tsx
@@ -9,7 +9,16 @@ export default function ProductThumbnail({
 }) {
   return (
     <div>
-      <img width="100%" src={img} alt={alt} title={title} />
+      <div className="relative">
+        <img src={img} alt={alt} className="w-full h-full object-cover" />
+        <div className="grid grid-col-1 gap-4 absolute inset-0 bg-black bg-opacity-50 opacity-0 md:hover:opacity-100 transition ease-in duration-75 place-content-center">
+          <button className="btn btn-primary lowercase w-40">view</button>
+          <button className="btn btn-primary lowercase w-40">
+            add to cart
+          </button>
+        </div>
+      </div>
+      {/* <img width="100%" src={img} alt={alt} title={title} /> */}
     </div>
   );
 }
