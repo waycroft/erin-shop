681cb3ebf38c5818481b9f3d27095ceed2024243
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2022-10-16 16:51:20 -0700

    product thumbnail tweaks (btn secondary, fallback text)

--- app/components/ProductThumbnail.tsx
+++ app/components/ProductThumbnail.tsx
@@ -12,7 +12,7 @@ export default function ProductThumbnail({
       <div className="relative">
         <img src={img} alt={alt} className="w-full h-full object-cover" />
         <div className="grid grid-col-1 gap-4 absolute inset-0 bg-black bg-opacity-50 opacity-0 md:hover:opacity-100 transition ease-in duration-75 place-content-center">
-          <button className="btn btn-primary lowercase w-40">view</button>
+          <button className="btn btn-secondary lowercase w-40">view</button>
           <button className="btn btn-primary lowercase w-40">
             add to cart
           </button>
--- app/components/ProductsGallery.tsx
+++ app/components/ProductsGallery.tsx
@@ -7,7 +7,17 @@ export default function ProductsGallery({ products }: { products: Product[] }) {
     <div className="bg-emerald-300 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-8">
       {products.map((product) => (
         <Link to={`/piece/${product.handle}`}>
-          <ProductThumbnail key={product.id} img={product.featuredImage.url} />
+          {product &&
+          (product.featuredImage || product.images?.edges[0]?.node) ? (
+            <ProductThumbnail
+              key={product.id}
+              img={
+                product.featuredImage.url ?? product.images.edges[0].node.url
+              }
+            />
+          ) : (
+            <div>(Missing product thumbnail</div>
+          )}
           {/* TODO: delete this line */}
           {/* <caption className="text-center text-white">{product.handle}</caption> */}
         </Link>
