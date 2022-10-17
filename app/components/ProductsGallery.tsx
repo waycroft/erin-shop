6dfccface4fc088be1e664ab1df2c9733fd91955
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2022-10-16 19:30:54 -0700

    add key attribute to Link

--- app/components/ProductsGallery.tsx
+++ app/components/ProductsGallery.tsx
@@ -6,7 +6,7 @@ export default function ProductsGallery({ products }: { products: Product[] }) {
   return (
     <div className="bg-emerald-300 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-8">
       {products.map((product) => (
-        <Link to={`/piece/${product.handle}`}>
+        <Link to={`/piece/${product.handle}`} key={product.id}>
           {product &&
           (product.featuredImage || product.images?.edges[0]?.node) ? (
             <ProductThumbnail
