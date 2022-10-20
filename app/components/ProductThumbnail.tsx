98a87bc76f297ba24ab586496f0d7af63a5f348b
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2022-10-19 20:16:25 -0700

    Don't wrap gallery thumbnail in <Link> for now, just use the "view" button so that "add to cart" button isn't overlaid by the <Link>

--- app/components/ProductThumbnail.tsx
+++ app/components/ProductThumbnail.tsx
@@ -1,24 +1,27 @@
-import { useFetcher } from "@remix-run/react";
+import { Link, useFetcher } from "@remix-run/react";
 
 export default function ProductThumbnail({
   img,
   alt,
   title,
   productVariantId,
+  productSlug,
 }: {
   img: string;
   alt?: string;
   title?: string;
   productVariantId: string;
+  productSlug: string;
 }) {
   const fetcher = useFetcher();
   return (
     <div className="relative">
       <img src={img} alt={alt} className="w-full h-full object-cover" />
       <div className="grid grid-col-1 gap-4 absolute inset-0 bg-black bg-opacity-50 opacity-0 md:hover:opacity-100 transition ease-in duration-75 place-content-center">
-        <button className="btn btn-secondary lowercase w-40">view</button>
+        <Link to={`/piece/${productSlug}`}>
+          <button className="btn btn-secondary lowercase w-40">view</button>
+        </Link>
         <fetcher.Form method="post" action="/cart">
-          {/* BOOKMARK: The link in the parent route is preventing this form from submitting */}
           <input type="hidden" name="merchandiseId" value={productVariantId} />
           <input type="hidden" name="quantity" value="1" />
           <button
--- app/components/ProductsGallery.tsx
+++ app/components/ProductsGallery.tsx
@@ -1,26 +1,21 @@
-import { Link } from "@remix-run/react";
 import { Product } from "~/routes/piece/$productHandle";
 import ProductThumbnail from "./ProductThumbnail";
 
 export default function ProductsGallery({ products }: { products: Product[] }) {
   return (
     <div className="bg-emerald-300 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-8">
-      {products.map((product) => (
-        <Link to={`/piece/${product.handle}`} key={product.id}>
-          {product &&
-          (product.featuredImage || product.images?.edges[0]?.node) ? (
-            <ProductThumbnail
-              key={product.id}
-              productVariantId={product.variants.edges[0].node.id}
-              img={
-                product.featuredImage.url ?? product.images.edges[0].node.url
-              }
-            />
-          ) : (
-            <div>(Missing product thumbnail</div>
-          )}
-        </Link>
-      ))}
+      {products.map((product) =>
+        product && (product.featuredImage || product.images?.edges[0]?.node) ? (
+          <ProductThumbnail
+            key={product.id}
+            productVariantId={product.variants.edges[0].node.id}
+            productSlug={product.handle}
+            img={product.featuredImage.url ?? product.images.edges[0].node.url}
+          />
+        ) : (
+          <div>(Missing product thumbnail</div>
+        )
+      )}
     </div>
   );
 }
