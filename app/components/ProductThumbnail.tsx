a5abb2348f988fe50351a927fb5d418e9cbf516e
Tyler Termini <69917602+waycroft@users.noreply.github.com>
2022-11-20 11:45:03 -0800

    remove the error handling pattern on the hover buttons for now (it's using the error boundary for server errors. Later I can handle a specific shopify error and prompt the user to try again)

--- app/components/ProductThumbnail.tsx
+++ app/components/ProductThumbnail.tsx
@@ -4,28 +4,11 @@ import { motion } from "framer-motion";
 import { useContext } from "react";
 import { CartIdContext } from "~/utils/cartContext";
 
-function ErrorButton({ fetcher }: { fetcher: FetcherWithComponents<any> }) {
-  return (
-    <button
-      type="submit"
-      name="_action"
-      value="addLineItem"
-      className="btn btn-primary lowercase w-40 h-fit leading-4 py-4 bg-error hover:bg-error border-none font-bold"
-    >
-      {fetcher.state !== "idle"
-        ? "Trying..."
-        : "Oh no, couldn't add to cart. 😢 Try again?"}
-    </button>
-  );
-}
-
 function ProductHoverActionButtons({
-  addToCartFailed,
   productSlug,
   productVariantId,
   fetcher,
 }: {
-  addToCartFailed: boolean;
   productSlug: string;
   productVariantId: string;
   fetcher: FetcherWithComponents<any>;
@@ -38,33 +21,17 @@ function ProductHoverActionButtons({
         <button className="btn btn-outline lowercase w-40">view</button>
       </Link>
       <fetcher.Form method="post" action="/">
-        <input
-          type="hidden"
-          name="cartId"
-          value={cartId}
-        />
-        <input
-          type="hidden"
-          name="merchandiseId"
-          value={productVariantId}
-        />
-        <input
-          type="hidden"
-          name="quantity"
-          value={1}
-        />
-        {!!addToCartFailed ? (
-          <ErrorButton fetcher={fetcher} />
-        ) : (
-          <button
-            type="submit"
-            className="btn lowercase w-40"
-            name="_action"
-            value="addLineItems"
-          >
-            {fetcher.state !== "idle" ? "added!" : "add to cart"}
-          </button>
-        )}
+        <input type="hidden" name="cartId" value={cartId} />
+        <input type="hidden" name="merchandiseId" value={productVariantId} />
+        <input type="hidden" name="quantity" value={1} />
+        <button
+          type="submit"
+          className="btn lowercase w-40"
+          name="_action"
+          value="addLineItems"
+        >
+          {fetcher.state !== "idle" ? "added!" : "add to cart"}
+        </button>
       </fetcher.Form>
     </div>
   );
@@ -72,7 +39,7 @@ function ProductHoverActionButtons({
 
 const variants = {
   visible: { opacity: 1, x: 0, y: 0 },
-  hidden: { opacity: 0, x: -15, y: -7.5},
+  hidden: { opacity: 0, x: -15, y: -7.5 },
 };
 
 export default function ProductThumbnail({
@@ -89,10 +56,7 @@ export default function ProductThumbnail({
   const fetcher = useFetcher();
 
   return (
-    <motion.div
-      className="relative"
-      variants={variants}
-    >
+    <motion.div className="relative" variants={variants}>
       <div>
         <Link to={`/piece/${productSlug}`}>
           <img
@@ -104,26 +68,17 @@ export default function ProductThumbnail({
       </div>
       <div className="hidden md:grid grid-col-1 gap-4 absolute inset-0 bg-base-100 bg-opacity-70 opacity-0 md:hover:opacity-100 transition ease-in duration-75 place-content-center rounded-lg">
         <ProductHoverActionButtons
-          addToCartFailed={fetcher.type === "done" && fetcher.data}
           productSlug={productSlug}
           productVariantId={productVariantId}
           fetcher={fetcher}
         />
       </div>
       {fetcher.type === "done" ? (
-        fetcher.data ? (
-          <ToastNotification
-            type="error"
-            message="Whoops, something went wrong. Please try again."
-            action={{ label: "Go to cart", href: "/cart" }}
-          />
-        ) : (
-          <ToastNotification
-            type="success"
-            message="Added to cart!"
-            action={{ label: "Go to cart", href: "/cart" }}
-          />
-        )
+        <ToastNotification
+          type="success"
+          message="Added to cart!"
+          action={{ label: "Go to cart", href: "/cart" }}
+        />
       ) : null}
     </motion.div>
   );
